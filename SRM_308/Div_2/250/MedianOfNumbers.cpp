39a4644e25db09825bed52a920364fba4ee83a52
Jackson Collier <jcolli44@vols.utk.edu>
2015-10-04 20:43:44 -0400

    Adding solution to Topcoder SRM 308, Div 2, 250 pointer, "MedianOfNumbers"

---
+++ SRM_308/Div_2/250/MedianOfNumbers.cpp
@@ -0,0 +1,26 @@
+#include <set>
+#include <map>
+#include <list>
+#include <queue>
+#include <stack>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class MedianOfNumbers{
+    public:
+        int findMedian(vector <int> numbers);
+};
+
+int MedianOfNumbers::findMedian(vector <int> numbers){
+	
+	if (numbers.size() % 2 == 0){
+		return -1;
+	}
+
+	sort(numbers.begin(), numbers.end());
+
+	return numbers[numbers.size() / 2];	
+}
