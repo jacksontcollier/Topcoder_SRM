69ebc6120396e4dd5ad0eb2e7350d29524789959
Jackson Collier <jcolli44@vols.utk.edu>
2015-10-06 17:48:31 -0400

    Adding solution to SRM 439, Div 2, 250 pointer, "Square of Digits"

---
+++ SRM_439/Div_2/250/SquareOfDigits.cpp
@@ -0,0 +1,39 @@
+#include <set>
+#include <map>
+#include <list>
+#include <queue>
+#include <stack>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class SquareOfDigits{
+    public:
+        int getMax(vector <string> data);
+};
+
+int SquareOfDigits::getMax(vector <string> data){
+	int i = 0;					// Arbitrary iteration variable
+	int j = 0;					// Arbitrary iteration variable
+	int k = 0;					// Arbitrary iteration variable
+	int max_area = 1;			// Area of largest square with same corner pieces
+
+	for (i = 0; i < data.size(); i++){
+		for (j = 0; j < data[i].size(); j++){
+			for (k = j + 1; k < data[i].size(); k++){
+				if (data[i][j] == data[i][k]){
+					if (i + (k - j) < data.size()){
+						if ((data[i + (k-j)][j] == data[i][j]) && (data[i + (k-j)][k] == data[i][j])){
+							if (((k - j) + 1) * ((k - j) + 1) > max_area){
+								max_area = ((k - j) + 1) * ((k - j) + 1);
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return max_area;
+}
