cb054a10be8f7746bb8a62b8ac466687cc801813
Jackson Collier <jacksontcollier@gmail.com>
2017-03-05 22:32:13 -0500

    Refactor topcoder srm 176, div 2, 250 solution
    Add ShouldFormAlternateComplement, a member function that encapsulates logic for determining whether the normal complement or alternate complement should be formed for a set of color values
    
    Add FormComplement, a member function that forms the normal complement for the given rgb values
    
    Add FormAlternateComplement, a member function that forms the alternate complement for the given rgb values
    
    Use ShouldFormAlternateComplement, FormComplement and FormAlternateComplement in getComplement

--- SRM_176/Div_2/250/RGBColor.cpp
+++ SRM_176/Div_2/250/RGBColor.cpp
@@ -10,26 +10,55 @@ class RGBColor
 {
 public:
   std::vector<int> getComplement(std::vector<int> rgb);
+
+private:
+  std::vector<int> FormComplement(std::vector<int>& component_values);
+  std::vector<int> FormAlternateComplement(std::vector<int>& component_values);
+  bool ShouldFormAlternateComplement(std::vector<int>& component_values);
 };
 
 std::vector<int> RGBColor::getComplement(std::vector<int> rgb)
 {
-  bool should_form_alternate_complement = true;
-  std::vector<int> complement;
-
-  for (int i = 0; i < rgb.size(); i++) {
-    int component_complement = MAX_COLOR_VALUE - rgb[i];
-    if (component_complement > MIN_DISTINGUISH_RANGE) {
-      should_form_alternate_complement = false;
-    }
-    complement.push_back(component_complement);
+  if (ShouldFormAlternateComplement(rgb)) {
+    return FormAlternateComplement(rgb);
   }
 
-  if (should_form_alternate_complement) {
-    for (int i = 0; i < complement.size(); i++) {
-      complement[i] = std::max(rgb[i] - MEDIAN_COLOR_VALUE, rgb[i] + MEDIAN_COLOR_VALUE);
-    }
+  return FormComplement(rgb);
+}
+
+std::vector<int> RGBColor::FormComplement(std::vector<int>& component_values)
+{
+  std::vector<int> complement;
+
+  for (auto component = component_values.begin(); component != component_values.end();
+      component++) {
+    complement.push_back(MAX_COLOR_VALUE - *component);
   }
 
   return complement;
+}
+
+std::vector<int> RGBColor::FormAlternateComplement(std::vector<int>& component_values)
+{
+  std::vector<int> alternate_complement;
+
+  for (auto component = component_values.begin(); component != component_values.end();
+      component++) {
+    alternate_complement.push_back(*component >= MEDIAN_COLOR_VALUE ?
+        *component - MEDIAN_COLOR_VALUE : *component + MEDIAN_COLOR_VALUE);
+  }
+
+  return alternate_complement;
+}
+
+bool RGBColor::ShouldFormAlternateComplement(std::vector<int>& component_values)
+{
+  for (auto component = component_values.begin(); component != component_values.end();
+      component++) {
+    if (std::abs(*component - (MAX_COLOR_VALUE - *component)) > MIN_DISTINGUISH_RANGE) {
+      return false;
+    }
+  }
+
+  return true;
 }
